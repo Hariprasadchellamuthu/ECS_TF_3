a4321b34f9bb59a06199880b25e9e13fe6c80401
Hariprasadchellamuthu <83823076+Hariprasadchellamuthu@users.noreply.github.com>
2023-12-25 08:43:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,132 +2,53 @@ provider "aws" {
   region = "ap-south-1"  # Replace with your AWS region
 }
 
-resource "aws_iam_role_policy" "ecs_execution_role_policy" {
-  name   = "ecs_execution_role_policy"
-  role   = aws_iam_role.ecs_execution_role.id
+# Create a standard label resource. See [null-label](https://github.com/cloudposse/terraform-null-label/#terraform-null-label--)
+module "label" {
+  source  = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version, though usually you want to use the current one
+  # version = "x.x.x"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "ec2:CreateTags",
-          "ec2:RunInstances",
-          "ec2:StopInstances",
-          "ec2:StartInstances",
-          "ec2:TerminateInstances",
-          # Add other EC2 related actions as necessary
-        ],
-        Resource = "*",
-      },
-    ],
-  })
+  namespace = "eg"
+  name      = "example"
 }
 
-resource "aws_iam_role" "ecs_task_role" {
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17",
-    Statement = [
-      {
-        Action    = "sts:AssumeRole",
-        Effect    = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
+module "vpc" {
+  source  = "cloudposse/vpc/aws"
+  version = "1.2.0"
+
+  context                 = module.label.context
+  ipv4_primary_cidr_block = "172.16.0.0/16"
 }
 
-resource "aws_iam_role_policy" "ecs_task_role_policy" {
-  name   = "ecs_task_role_policy"
-  role   = aws_iam_role.ecs_task_role.id
+module "subnets" {
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "2.0.4"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = [
-          "iam:PassRole",
-          "iam:CreateRole",
-          "iam:AttachRolePolicy",
-          "iam:PutRolePolicy",
-          "iam:DeleteRolePolicy",
-          "iam:DeleteRole",
-          # Add other IAM related actions as necessary
-          "ec2:DescribeSecurityGroups",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:RevokeSecurityGroupIngress",
-          # Add other security group related actions as necessary
-        ],
-        Resource = "*",
-      },
-    ],
-  })
+  context              = module.label.context
+  availability_zones   = var.availability_zones
+  vpc_id               = module.vpc.vpc_id
+  igw_id               = [module.vpc.igw_id]
+  ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
+  nat_gateway_enabled  = True
+  nat_instance_enabled = True
 }
 
-resource "aws_ecs_cluster" "python_cluster" {
-  name = "python-ecs-cluster"
-}
+module "ecs_cluster" {
+  source = "cloudposse/ecs-cluster/aws"
 
-resource "aws_launch_configuration" "ecs_launch_configuration" {
-  name                 = "ecs-launch-config"
-  image_id             = "ami-0aee0743bf2e81172"  # Replace with your AMI ID
-  instance_type        = "t2.small" 
+  context = module.label.context
 
-}
-
-resource "aws_autoscaling_group" "ecs_autoscaling_group" {
-  desired_capacity     = 1 
-  max_size             = 3 
-  min_size             = 1 
-
-  launch_configuration = aws_launch_configuration.ecs_launch_configuration.id
- 
-}
-
-resource "aws_ecs_task_definition" "python_task_definition" {
-  family                   = "python-task-family"
-  network_mode             = "bridge"  # Using bridge network mode for EC2 launch type
-
-  cpu    = "512"   # 0.5 vCPU
-  memory = "1024"  # 1GB
-
-  execution_role_arn = aws_iam_role.ecs_execution_role.arn
-  task_role_arn      = aws_iam_role.ecs_task_role.arn
-
-  container_definitions = jsonencode([
-    {
-      name  = "python-container"
-      image = "amazonlinux:latest"
-      cpu   = 512
-      memory = 1024
-      essential = true
-      command = [
-        "/bin/bash",
-        "-c",
-        "yum update -y && yum install -y python3.6"  # Replace with your Python script
-      ]
+  container_insights_enabled      = true
+  capacity_providers_fargate      = true
+  capacity_providers_fargate_spot = true
+  capacity_providers_ec2 = {
+    default = {
+      instance_type               = "t3.medium"
+      security_group_ids          = [module.vpc.vpc_default_security_group_id]
+      subnet_ids                  = module.subnets.private_subnet_ids
+      associate_public_ip_address = True
+      min_size                    = 0
+      max_size                    = 2
     }
-  ])
-}
-
-resource "aws_ecs_service" "python_ecs_service" {
-  name            = "python-ecs-service"
-  cluster         = aws_ecs_cluster.python_cluster.id
-  task_definition = aws_ecs_task_definition.python_task_definition.arn
-  launch_type     = "EC2"
-
-  # No need to specify network_configuration when using bridge mode with default VPC
-  # deployment_controller block remains the same
-  deployment_controller {
-    type = "ECS"
   }
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [aws_ecs_task_definition.python_task_definition]
 }
